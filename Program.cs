ae2c688c79e0744f0d43f94cc33e15122ddce2d0
Huy Truong Duc <144222775+huyhandsome2k4@users.noreply.github.com>
2023-10-08 23:46:30 +0700

    HomeWork4

--- Program.cs
+++ Program.cs
@@ -1,67 +1,153 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework2
+namespace HW4
 {
-    class Person
+    class Program
     {
-        private string name { get; set; }
-        private string address { get; set; }
-        private double salary { get; set; }
-        private List<Person> list = new List<Person>();
-        public  void  InputPersonInfo()
-        {
-            
-            Console.WriteLine("Nhập số lượng sinh viên:");
-            int soluong = int.Parse(Console.ReadLine());
-            for (int i = 0; i <soluong; i++)
-            {
-                Person person = new Person();
-                Console.Write("Nhập tên:");
-                person.name = Console.ReadLine();
-                Console.Write("Nhập địa chỉ:");
-                person.address = Console.ReadLine();
-                do
-                {
-                    Console.Write("Nhập lương: ");
-                    person.salary = double.Parse(Console.ReadLine());
-
-                    if (person.salary <= 0)
-                    {
-                        Console.WriteLine("Lương không hợp lệ, vui lòng nhập lại!");
-                    }
-                } while (person.salary <= 0);
-                list.Add(person);    
-            }
-
-
-
-        }
-        public void DisplayPersonInfo()
-        {
-            list.Sort((x, y) => x.salary.CompareTo(y.salary));
-            Console.WriteLine("Thông tin sinh viên:");
-            foreach (Person person in list)
-            {
-                Console.WriteLine($"Họ tên: {person.name}");
-                Console.WriteLine($"Địa chỉ: {person.address}");
-                Console.WriteLine($"Lương: {person.salary}");
-            }
-        }
-        
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Person person = new Person();
-            person.InputPersonInfo();
-            person.DisplayPersonInfo();
-            
+            NganHang nganHang = new NganHang();
 
-            
+            // Mở tài khoản
+            TaiKhoan taiKhoan1 = nganHang.MoTaiKhoan("123456789", "Nguyen Van A", "0123456789");
+            TaiKhoan taiKhoan2 = nganHang.MoTaiKhoan("987654321", "Nguyen Thi B", "9876543210");
+
+            // Nạp tiền
+            taiKhoan1.NapTien(1000000);
+            taiKhoan2.NapTien(5000000);
+
+            // Rút tiền
+            taiKhoan1.RutTien(500000);
+            taiKhoan2.RutTien(2000000);
+
+            // Tạo báo cáo
+            BaoCaoNganHang baoCao = new BaoCaoNganHang(nganHang);
+            baoCao.TaoBaoCao();
+
+            Console.ReadLine();
+        }
+    }
+
+    class NganHang
+    {
+        private List<TaiKhoan> danhSachTaiKhoan;
+
+        public NganHang()
+        {
+            danhSachTaiKhoan = new List<TaiKhoan>();
+        }
+
+        public TaiKhoan MoTaiKhoan(string soTaiKhoan, string tenChuSoHuu, string cmnd)
+        {
+            TaiKhoan taiKhoan = new TaiKhoan(soTaiKhoan, tenChuSoHuu, cmnd);
+            danhSachTaiKhoan.Add(taiKhoan);
+            return taiKhoan;
+        }
+
+        public List<TaiKhoan> LayDanhSachTaiKhoan()
+        {
+            return danhSachTaiKhoan;
+        }
+    }
+
+    class TaiKhoan
+    {
+        public string SoTaiKhoan { get; }
+        public string TenChuSoHuu { get; }
+        public string CMND { get; }
+        public decimal SoDu { get; private set; }
+
+        private List<GiaoDich> danhSachGiaoDich;
+
+        public TaiKhoan(string soTaiKhoan, string tenChuSoHuu, string cmnd)
+        {
+            SoTaiKhoan = soTaiKhoan;
+            TenChuSoHuu = tenChuSoHuu;
+            CMND = cmnd;
+            SoDu = 0;
+            danhSachGiaoDich = new List<GiaoDich>();
+        }
+
+        public void NapTien(decimal soTien)
+        {
+            if (soTien > 0)
+            {
+                SoDu += soTien;
+                GiaoDich giaoDich = new GiaoDich(DateTime.Now, GiaoDich.LoaiGiaoDich.NapTien, soTien);
+                danhSachGiaoDich.Add(giaoDich);
+                Console.WriteLine("Nạp tiền thành công. Số dư mới: " + SoDu);
+            }
+            else
+            {
+                Console.WriteLine("Số tiền nạp vào phải lớn hơn 0.");
+            }
+        }
+
+        public void RutTien(decimal soTien)
+        {
+            if (soTien > 0 && SoDu >= soTien)
+            {
+                SoDu -= soTien;
+                GiaoDich giaoDich = new GiaoDich(DateTime.Now, GiaoDich.LoaiGiaoDich.RutTien, soTien);
+                danhSachGiaoDich.Add(giaoDich);
+                Console.WriteLine("Rút tiền thành công. Số dư mới: " + SoDu);
+            }
+            else
+            {
+                Console.WriteLine("Số tiền rút không hợp lệ hoặc không đủ số dư.");
+            }
+        }
+
+        public void InThongTinTaiKhoan()
+        {
+            Console.WriteLine("Số tài khoản: " + SoTaiKhoan);
+            Console.WriteLine("Tên chủ sở hữu: " + TenChuSoHuu);
+            Console.WriteLine("Số dư: " + SoDu);
+            Console.WriteLine("-------------------------");
+        }
+    }
+
+    class GiaoDich
+    {
+        public enum LoaiGiaoDich
+        {
+            NapTien,
+            RutTien
+        }
+
+        public DateTime ThoiGian { get; }
+        public LoaiGiaoDich Loai { get; }
+        public decimal SoTien { get; }
+
+        public GiaoDich(DateTime thoiGian, LoaiGiaoDich loai, decimal soTien)
+        {
+            ThoiGian = thoiGian;
+            Loai = loai;
+            SoTien = soTien;
+        }
+    }
+
+    class BaoCaoNganHang
+    {
+        private NganHang nganHang;
+
+        public BaoCaoNganHang(NganHang nganHang)
+        {
+            this.nganHang = nganHang;
+        }
+
+        public void TaoBaoCao()
+        {
+            List<TaiKhoan> danhSachTaiKhoan = nganHang.LayDanhSachTaiKhoan();
+
+            foreach (TaiKhoan taiKhoan in danhSachTaiKhoan)
+            {
+                taiKhoan.InThongTinTaiKhoan();
+            }
         }
     }
 }
